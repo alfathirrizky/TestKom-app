8691faec09b6cd50edc6bf8e9041d23cc799acff
alfathir rizky harsya <alfathirrizky083@gmail.com>
2026-02-18 22:21:51 +0700

    add: SideBar Blade

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -1,20 +1,43 @@
-<div>
-    <div>
-        <h1>Ini Sidebar</h1>
-        <img src="" alt="" srcset="">
-    </div>
-    <div>
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Contact</a></li>
-        </ul>
-    </div>
-    <div class="flex gap-2">
-        <div>
-            <img src="" alt="profile" srcset="">
-            <h1>profile</h1>
+<div x-data="{ open: true }" class="flex">
+    <!-- Sidebar -->
+    <aside 
+        :class="open ? 'w-64' : 'w-20'" 
+        class="bg-gray-900 text-white min-h-screen transition-all duration-300">
+
+        <!-- Logo -->
+        <div class="flex items-center justify-between p-4 border-b border-gray-700">
+            <span x-show="open" class="text-xl font-bold">MyApp</span>
+            <button @click="open = !open" class="text-gray-400 hover:text-white">
+                ☰
+            </button>
         </div>
-        <button>Logout</button>
-    </div>
+
+        <!-- Menu -->
+        <nav class="mt-4">
+            <a href="#" class="flex items-center p-3 hover:bg-gray-800 transition">
+                <span class="text-lg">🏠</span>
+                <span x-show="open" class="ml-3">Dashboard</span>
+            </a>
+
+            <a href="#" class="flex items-center p-3 hover:bg-gray-800 transition">
+                <span class="text-lg">📊</span>
+                <span x-show="open" class="ml-3">Laporan</span>
+            </a>
+
+            <a href="#" class="flex items-center p-3 hover:bg-gray-800 transition">
+                <span class="text-lg">⚙️</span>
+                <span x-show="open" class="ml-3">Pengaturan</span>
+            </a>
+
+            <a href="#" class="flex items-center p-3 hover:bg-gray-800 transition">
+                <span class="text-lg">🚪</span>
+                <span x-show="open" class="ml-3">Logout</span>
+            </a>
+        </nav>
+    </aside>
+
+    <!-- Content -->
+    <main class="flex-1 bg-gray-100 min-h-screen p-6">
+        {{ $slot }}
+    </main>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -273,5 +273,6 @@
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
+        <script src="//unpkg.com/alpinejs" defer></script>
     </body>
 </html>
