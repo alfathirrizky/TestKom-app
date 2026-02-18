28f4dc26ff519f2a716836cbfca49cd2442675f2
alfathir rizky harsya <alfathirrizky083@gmail.com>
2026-02-18 22:18:46 +0700

    add: SideBar Blade

---
+++ resources/views/components/side-bar.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <div>
+        <h1>Ini Sidebar</h1>
+        <img src="" alt="" srcset="">
+    </div>
+    <div>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </div>
+    <div class="flex gap-2">
+        <div>
+            <img src="" alt="profile" srcset="">
+            <h1>profile</h1>
+        </div>
+        <button>Logout</button>
+    </div>
+</div>
